33149d4e0f7f7a8f0b16cc2321169cbc5b15f7e5
SaikaOS <200103235@stu.sdu.edu.kz>
2022-07-27 23:15:31 +0600

    commit for farm contrib

--- layouts/header.tsx
+++ layouts/header.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import {BsSearch} from 'react-icons/bs'
 import HeaderNav from '../components/header-nav'
+import { useGetNewsFromCategoryQuery } from '../services/news'
 
 const Header = () => {
+  const [searchText, setSearchText] = useState('')
+  const {data, isLoading} = useGetNewsFromCategoryQuery('')
+
+  const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value)
+  }
   return (
     <div className='h-20 flex justify-between items-center'>
         <input 
         type="text" 
         placeholder='Search anything...' 
-        className='w-[300px] p-2 rounded-xl relative outline-none'/>
+        className='w-[300px] p-2 rounded-xl relative outline-none'
+        value={searchText}
+        onChange={handleChange}
+        />
       <BsSearch className='absolute left-10'/>
         <HeaderNav />
     </div>
